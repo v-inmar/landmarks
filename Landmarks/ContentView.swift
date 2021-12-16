aad657fd44ff84967d9ceb762da82a1b491d2b4a
Vinmar Tipay <vinmar.tipay@outlook.com>
2021-12-16 21:52:50 +0000

    Added comments and extra preview. Also changed the initial text

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -7,15 +7,28 @@
 
 import SwiftUI
 
+// Struct conforming to View protocol
+// Shows and describe the layout of the app's contents
 struct ContentView: View {
     var body: some View {
-        Text("Test!")
+        Text("This is SwiftUi: Landmarks")
             .padding()
     }
 }
 
+
+
+
+
+// Handles with the preview
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark) // this inherits dark mode
+        
+        // Allows multiple previews to check for app's
+        // visual with other modes
+        ContentView()
+            .preferredColorScheme(.light) // light mode
     }
 }
