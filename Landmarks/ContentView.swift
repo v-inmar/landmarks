9c8337dc0e71ad6a01bb99901124ddbee02a1a21
Vinmar Tipay <vinmar.tipay@outlook.com>
2021-12-17 00:05:38 +0000

    Check your understanding

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -64,3 +64,122 @@ struct ContentView_Previews: PreviewProvider {
             .preferredColorScheme(.dark) // light mode
     }
 }
+
+
+// ---- Check Your Understanding ---- //
+
+
+
+/*
+ Question 1 of 4
+ When creating a custom SwiftUI view, where do you declare the view's layout?
+ 
+ Answer:
+ In the body property
+ 
+ Comment:
+ Custom views implement the body property, which is a requirement of the View protocol
+ */
+
+
+
+
+
+
+/*
+ Question 2 of 4
+ Which layout renders from the following view code?
+ 
+ var body: some View {
+     HStack {
+         CircleImage()
+         VStack(alignment: .leading) {
+             Text("Turtle Rock")
+                 .font(.title)
+             Text("Joshua Tree National Park")
+         }
+     }
+ }
+ 
+ Answer:
+ 3rd option
+ The Image is to the left of the texts
+ 
+ Comment:
+ The nested horizontal and vertical stacks arrange the image to the left of the two text views.
+ */
+
+
+
+
+
+
+/*
+ Question 3 of 4
+ Which of these is a correct way to return three views from a custom view’s body property?
+ 
+ Option 1:
+ VStack {
+    Text("Turtle Rock")
+       .font(.title)
+    Divider()
+    Text("Joshua Tree National Park")
+ }
+ 
+ 
+ Option 2:
+ [
+     Text("Turtle Rock").font(.title),
+     Divider(),
+     Text("Joshua Tree National Park")
+ ]
+ 
+ 
+ Option 3:
+ Text("Turtle Rock")
+     + Divider()
+     + Text("Joshua Tree National Park")
+ 
+ Answer:
+ 1st option
+ body property can, at most, return single view. Stacking the views allows multiple views to be displayed
+ 
+ Comment:
+ You can use a stack to return multiple views from a body property.
+ */
+
+
+
+
+
+
+/*
+ Question 4 of 4
+ Which is the correct way to use modifier methods to configure a view?
+ 
+ Option 1:
+ var text = Text("Hello world!")
+ text.font(.title)
+ text.foregroundColor(.purple)
+ return text
+ 
+ 
+ Option 2:
+ var text = Text("Hello world!")
+ text.font = .title
+ text.foregroundColor = .purple
+ return text
+ 
+ 
+ Option 3:
+ Text("Hello world!")
+    .font(.title)
+    .foregroundColor(.purple)
+ 
+ 
+ Answer:
+ 3rd option
+ 
+ Comment:
+ A modifier returns a view that applies a new behavior or visual change. You can chain multiple modifiers to achieve the effects you need.
+ */
